8dc2efd8898877c2bfc2b04bbe183053b65ba861
Anam Shah <anam.digicom@gmail.com>
2018-08-28 12:26:32 +0530

    Bug fix

--- Pieces/King.php
+++ Pieces/King.php
@@ -20,7 +20,7 @@ class King extends ChessBoard
         for ($i = -1; $i <= 1; $i++) {
             for ($j = -1; $j <= 1; $j++) {
                 if ($row - $i != 0)
-                    if ($col - $j != 0)
+                    if ($col - $j > 0 && $col - $j <= 8)
                         if (!($this->row_letters[$row - $i] == $this->row_letters[$row] && ($col - $j) == $col))
                             $this->valid_moves[] = $this->row_letters[$row - $i] . ($col - $j);
             }
